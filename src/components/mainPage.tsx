5bb90f10ec1fe1973be2cdb2c66bb58cb6519ab3
mirco123 <miroslav.liska@gmail.com>
2023-06-04 21:21:29 -0700

    refactoring to react, next part

--- src/components/mainPage.tsx
+++ src/components/mainPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './mainPage.module.css';
-import globalStyles from './global.module.css';
 
 function mainPage() {
     return (
@@ -23,74 +22,74 @@ function mainPage() {
                         ktoré potešia vašich
                         používatelov a podporia
                         rast vášho podnikania</h1>
-                    <p className="subTitle">Natívna vývojová agentúra, pripravená na návrh, vytvárať a rozvíjať
+                    <p className={styles.subTitle}>Natívna vývojová agentúra, pripravená na návrh, vytvárať a rozvíjať
                         softvérové aplikácie
                         svetovej úrovne.
                     </p>
-                    <div className="headerGifSmaller">
+                    <div className={styles.headerGifSmaller}>
                         <img src="./assets/appDesignAndDevelopment.gif" alt="Animation"
-                             className="headerGifImgSmaller"/>
+                             className={styles.headerGifImgSmaller}/>
                     </div>
-                    <button className="headerButton">Kontaktujte nás</button>
+                    <button className={styles.headerButton}>Kontaktujte nás</button>
                 </div>
-                <div className="headerGif">
-                    <img src="./assets/appDesignAndDevelopment.gif" alt="Animation" className="headerGifImg"/>
+                <div className={styles.headerGif}>
+                    <img src="./assets/appDesignAndDevelopment.gif" alt="Animation" className={styles.headerGifImg}/>
                 </div>
             </div>
-            <div className="sluzby">
-                <h1 className="sluzbyTitle">Naše služby</h1>
-                <img className="underLine" src="./assets/underLine.svg" alt="Underline"/>
-                <div className="sluzbyContainer">
-                    <div className="mobilneApps">
-                        <img src="./assets/mobilneApps.svg" className="beruska"/>
-                        <h2 className="mobilneAppsTitle">Vývoj
+            <div className={styles.sluzby}>
+                <h1 className={styles.sluzbyTitle}>Naše služby</h1>
+                <img className={styles.underLine} src="./assets/underLine.svg" alt="Underline"/>
+                <div className={styles.sluzbyContainer}>
+                    <div className={styles.mobilneApps}>
+                        <img src="./assets/mobilneApps.svg" className={styles.beruska}/>
+                        <h2 className={styles.mobilneAppsTitle}>Vývoj
                             mobilných aplikácií
                         </h2>
-                        <p className="mobilneAppsSubTitle">
+                        <p className={styles.mobilneAppsSubTitle}>
                             S vysokou odbornosťou a rýchlosťou sa špecializujeme na tvorbu kvalitných mobilných
                             aplikácií
                             prostredníctvom React Native pre systémy Android aj iOS.
                         </p>
-                        <a className="zistiViac">
+                        <a className={styles.zistiViac}>
                             Zisti viac ›
                         </a>
                     </div>
-                    <div className="weboveApps">
-                        <img src="./assets/weboveApps.svg" className="beruska"/>
-                        <h2 className="weboveAppsTitle">Vývoj
+                    <div className={styles.weboveApps}>
+                        <img src="./assets/weboveApps.svg" className={styles.beruska}/>
+                        <h2 className={styles.weboveAppsTitle}>Vývoj
                             webových aplikácií
                         </h2>
-                        <p className="weboveAppsSubTitle">
+                        <p className={styles.weboveAppsSubTitle}>
                             Vyvíjame jedinečné webové aplikácie s modernými prístupmi a najnovšími technológiami, aby
                             sme
                             vyhoveli potrebám vášho podnikania.
                         </p>
-                        <a className="zistiViac">
+                        <a className={styles.zistiViac}>
                             Zisti viac ›
                         </a>
                     </div>
-                    <div className="uiuxApps">
-                        <img src="./assets/uiuxApps.svg" className="beruska"/>
-                        <h2 className="uiuxAppsTitle">UI / UX Dizajn</h2>
-                        <p className="uiuxAppsSubTitle">
+                    <div className={styles.uiuxApps}>
+                        <img src="./assets/uiuxApps.svg" className={styles.beruska}/>
+                        <h2 className={styles.uiuxAppsTitle}>UI / UX Dizajn</h2>
+                        <p className={styles.uiuxAppsSubTitle}>
                             S veľkou vášňou sa venujeme tvorbe nádherných a praktických používateľských rozhraní, ktoré
                             poskytujú plynulý a intuitívny zážitok.
                         </p>
-                        <a className="zistiViac">
+                        <a className={styles.zistiViac}>
                             Zisti viac ›
                         </a>
                     </div>
                 </div>
             </div>
-            <div className="oNas">
-                <div className="oNasBg2"/>
-                <div className="iconEllipseContainer">
-                    <img src="./assets/iconEllipse.svg" alt="IconEllipse" className="iconBrandsLogo"/>
+            <div className={styles.oNas}>
+                <div className={styles.oNasBg2}/>
+                <div className={styles.iconEllipseContainer}>
+                    <img src="./assets/iconEllipse.svg" alt="IconEllipse" className={styles.iconBrandsLogo}/>
                 </div>
-                <h2 className="oNasTitle">
+                <h2 className={styles.oNasTitle}>
                     Prečo si na vytváranie krásnych aplikácií vybrať Appky?
                 </h2>
-                <p className="oNasSubTitle">
+                <p className={styles.oNasSubTitle}>
                     Appky, Vaša najlepšia voľba pre vytváranie mobilných, webových či interných aplikácií, ti prináša
                     priateľský
                     a profesionálny prístup. S vášňou vytvárame produkty pomocou React Native, čo nám umožňuje ponúknuť
